3783156fa8046f74b39d1da5cd931915c6bfcc95
rogerio.filho <rogerio.filho@softplan.com.br>
2015-08-26 14:15:06 -0300

    changed icon and added color picker

--- luminaria-arduino/luminariaRGB/luminariaRGB.ino
+++ luminaria-arduino/luminariaRGB/luminariaRGB.ino
@@ -2,40 +2,22 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-// MAC address from Ethernet shield sticker under board
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 0, 111); // IP address, may need to change depending on network
-EthernetServer server(80);  // create a server at port 80
+EthernetServer server(80);
 
-String HTTP_req;          // stores the HTTP request
-boolean LED_status = 0;   // state of LED, off by default
 int redPin = 5;
 int greenPin = 6;
 int bluePin = 9;
 char requestInfo[26];
 
 int fromHexaCharToInt(char hexa) {
-	if (hexa == 'a' || hexa == 'A') {
-		return 10;
-	}
-	else if (hexa == 'b' || hexa == 'B') {
-		return 11;
-	}
-	else if (hexa == 'c' || hexa == 'C') {
-		return 12;
-	}
-	else if (hexa == 'd' || hexa == 'D') {
-		return 13;
-	}
-	else if (hexa == 'e' || hexa == 'E') {
-		return 14;
-	}
-	else if (hexa == 'f' || hexa == 'F') {
-		return 15;
-	}
-	else {
-		return atoi(&hexa);
-	}
+	if (hexa == 'a' || hexa == 'A') { return 10; }
+	else if (hexa == 'b' || hexa == 'B') { return 11; }
+	else if (hexa == 'c' || hexa == 'C') { return 12;	}
+	else if (hexa == 'd' || hexa == 'D') { return 13;	}
+	else if (hexa == 'e' || hexa == 'E') { return 14;	}
+	else if (hexa == 'f' || hexa == 'F') { return 15;	}
+	else { return atoi(&hexa); }
 }
 
 int adjustColor(int color) {
@@ -44,46 +26,28 @@ int adjustColor(int color) {
 	return newColor;
 }
 
-
-
-
-
-void setup()
-{
+void setup(){
   pinMode(redPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  Ethernet.begin(mac);  // initialize Ethernet device
-  server.begin();           // start to listen for clients
-  Serial.begin(9600);       // for diagnostics
+  Ethernet.begin(mac);
+  server.begin();
+  Serial.begin(9600);
   Serial.println(Ethernet.localIP());
-
-
 }
 
 void changeColor(char request[]) {
-  int red = adjustColor(fromHexaCharToInt(request[9]) * 16 + fromHexaCharToInt(request[10]));
-  int green = adjustColor(fromHexaCharToInt(request[11]) * 16 + fromHexaCharToInt(request[12]));
-  int blue = adjustColor(fromHexaCharToInt(request[13]) * 16 + fromHexaCharToInt(request[14]));
-  red = 255 - red;
-  green = 255 - green;
-  blue = 255 - blue;
-  
-  Serial.println(red);
-  Serial.println(green);
-  Serial.println(blue);
-  
+  int red = 255 - adjustColor(fromHexaCharToInt(request[9]) * 16 + fromHexaCharToInt(request[10]));
+  int green = 255 - adjustColor(fromHexaCharToInt(request[11]) * 16 + fromHexaCharToInt(request[12]));
+  int blue = 255 - adjustColor(fromHexaCharToInt(request[13]) * 16 + fromHexaCharToInt(request[14]));
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);
 }
 
-
-void loop()
-{
-  EthernetClient client = server.available();  // try to get client
-
-  if (client) {  // got client?
+void loop(){
+  EthernetClient client = server.available();
+  if (client) {
     int i = 0;
     boolean currentLineIsBlank = true;
     while (client.connected()) {
@@ -93,37 +57,26 @@ void loop()
           requestInfo[i] = c;
           i++;
         }
-        //HTTP_req += c;
-
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");
           client.println();
-
           Serial.println(requestInfo);
           if (strncmp("GET /LED-", requestInfo, 9) == 0) {
             changeColor(requestInfo);
-            Serial.println("LED ACENDE");
-            delay(1000);
           }
-          HTTP_req = "";    // finished with request, empty string
           break;
         }
-        // every line of text received from the client ends with \r\n
         if (c == '\n') {
-          // last character on line of received text
-          // starting new line with next character read
           currentLineIsBlank = true;
         }
         else if (c != '\r') {
-          // a text character was received from client
           currentLineIsBlank = false;
         }
-      } // end if (client.available())
-    } // end while (client.connected())
-    delay(1);      // give the web browser time to receive the data
-    client.stop(); // close the connection
-  } // end if (client)
+      }
+    }
+    delay(1);
+    client.stop();
+  }
 }
--- luminaria/www/js/app.js
+++ luminaria/www/js/app.js
@@ -13,25 +13,11 @@ angular.module('starter', ['ionic'])
 }).controller('luminariaCtrl', function($scope,$http) {
     $scope.color = "FF0000";
     var turnLight = function(){
+      console.log($scope.color);
       $http.get("http://192.168.25.68/LED-"+$scope.color).success(function(response){
         $scope.color = response;
       }).error(function(){
         $scope.color = "DEU RUIM";
       })
     };
-
-    $scope.blueLight = function(){
-     // $scope.color = "blue";
-      turnLight();
-    }
-
-    $scope.greenLight = function(){
-      //$scope.color = "green";
-      turnLight();
-    }
-
-    $scope.redLight = function(){
-      //$scope.color = "red";
-      turnLight();
-    }
   });
