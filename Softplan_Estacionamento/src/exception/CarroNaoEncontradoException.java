7c6b0fbd29954a2b02bb01ccf7b91a2fa6f1596f
rogerio.filho <rogerio.filho@softplan.com.br>
2015-09-10 11:46:33 -0300

    commit anterior sem intenção removido.

--- Softplan_Estacionamento/src/exception/CarroNaoEncontradoException.java
+++
@@ -1,6 +0,0 @@
-package exception;
-
-
-public class CarroNaoEncontradoException extends Exception {
-
-}
--- Softplan_Estacionamento/src/exception/ForaDoHorarioDeFuncionamentoException.java
+++
@@ -1,6 +0,0 @@
-package exception;
-
-
-public class ForaDoHorarioDeFuncionamentoException extends Exception {
-
-}
